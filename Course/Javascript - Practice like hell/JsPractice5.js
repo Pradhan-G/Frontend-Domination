b0c1cdcedb425e499259fe445385e93d1061b93b
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-02-24 02:12:50 +0530

    Question 5 done JS Practice Like Hell

---
+++ Course/Javascript - Practice like hell/JsPractice5.js
@@ -0,0 +1,26 @@
+// Build a countdown timer that starts when a button is clicked and updates the display in real time.
+var input = document.querySelector("#timer-input");
+
+var start = document.querySelector("#timer-btn-start");
+var stop = document.querySelector("#timer-btn-stop");
+
+var mainDiv = document.querySelector("#timer");
+
+var int;
+start.addEventListener("click", () => {
+  var val = Number(input.value);
+
+  int = setInterval(() => {
+    mainDiv.innerHTML = "<h1>" + val + "</h1>";
+
+    if (val === 0) {
+      clearInterval(int);
+    }
+    val--;
+  }, 1000);
+  input.value = "";
+});
+
+stop.addEventListener("click", () => {
+  clearInterval(int);
+});
