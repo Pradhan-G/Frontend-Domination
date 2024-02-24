cac4ec8a1409f3fe24a4de45088d01e129f50a23
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-02-24 22:32:59 +0530

    added new timelines to make my animation not take up a lot of time

--- Course/GSAP, ScrollTrigger, LocoMotive/Project/script.js
+++ Course/GSAP, ScrollTrigger, LocoMotive/Project/script.js
@@ -1,6 +1,8 @@
 // const { default: gsap } = require("gsap");
 
 var tl = gsap.timeline();
+var tl2 = gsap.timeline();
+var tl3 = gsap.timeline();
 
 tl.from("#nav-part-1", {
   y: -50,
@@ -21,52 +23,53 @@ tl.from("#nav-part-3", {
   duration: 0.3,
 });
 
-tl.from(".main-text", {
+tl2.from(".main-text", {
   x: -500,
   opacity: 0,
   duration: 1,
+  delay: 0.5,
 });
 
-tl.to(".main-text", {
+tl2.to(".main-text", {
   zIndex: 5,
   duration: 0,
   size: 1.2,
 });
 
-tl.from("img", {
+tl2.from("img", {
   opacity: 0,
   rotate: 0,
   duration: 0.5,
   stagger: 0.3,
 });
 
-tl.to("#img1", {
+tl2.to("#img1", {
   rotate: 0,
   x: -750,
   opacity: 100,
   duration: 0.4,
 });
-tl.to("#img4", {
+tl2.to("#img4", {
   rotate: 0,
   x: 750,
   opacity: 100,
   duration: 0.4,
 });
 
-tl.to("#img2", {
+tl2.to("#img2", {
   rotate: 0,
   x: -250,
   opacity: 100,
   duration: 0.4,
 });
-tl.to("#img3", {
+tl2.to("#img3", {
   rotate: 0,
   x: 250,
   opacity: 100,
   duration: 0.4,
 });
 
-tl.to("img", {
+tl2.to("img", {
   filter: "blur(10px)",
   ease: "power1.out",
   duration: 0.3,
@@ -94,7 +97,7 @@ tl.to("img", {
 //   duration: 0.3,
 // });
 
-tl.from("span", {
+tl2.from("span", {
   color: "rgb(215, 215, 215)",
   duration: 0.2,
   stagger: 0.05,
@@ -102,8 +105,9 @@ tl.from("span", {
   repeat: -1,
 });
 
-tl.from("#footer", {
+tl3.from("#footer", {
   y: 100,
   opacity: 0,
   duration: 0.5,
+  delay: 0.5,
 });
