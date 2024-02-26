ffe2b41f7faf20d1fac19aceaf7d9fc614e4e76a
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-02-26 14:27:54 +0530

    Added Lenis Js for better scrolling

--- Course/Practice/ScrollTrigger Practice/script.js
+++ Course/Practice/ScrollTrigger Practice/script.js
@@ -1,3 +1,14 @@
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 // import Shery from "sheryjs";
 
 Shery.mouseFollower();
