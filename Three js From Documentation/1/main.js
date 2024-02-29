4dd8a52ba993c68855ff1da52d3e0c09dd4d04de
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-01 01:21:18 +0530

    Did some things, lets see what can be done next

--- Three js From Documentation/1/main.js
+++ Three js From Documentation/1/main.js
@@ -1,33 +1,99 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
+import WebGL from "./node_modules/three/examples/jsm/capabilities/WebGL.js";
+import { CSS3DRenderer } from "./node_modules/three/examples/jsm/renderers";
+function first() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
+  const geometry = new THREE.BoxGeometry(2, 1, 1);
 
-const geometry = new THREE.BoxGeometry(2, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
 
-const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-scene.add(cube);
+  camera.position.z = 5;
 
-camera.position.z = 5;
+  function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  if (WebGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    animate();
+  } else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById("container").appendChild(warning);
+  }
+}
 
+// first();
+
+function drawingLines() {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+  camera.position.set(0, 0, 100);
+  camera.lookAt(0, 0, 0);
+
+  const scene = new THREE.Scene();
+
+  const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+  const points = [];
+
+  points.push(new THREE.Vector3(-10, 0, 0));
+  points.push(new THREE.Vector3(0, 10, 0));
+  points.push(new THREE.Vector3(10, 0, 0));
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  const line = new THREE.Line(geometry, material);
+
+  scene.add(line);
   renderer.render(scene, camera);
 }
-animate();
+
+// drawingLines()
+
+function TextRenderer3d() {
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+  camera.position.set(0, 0, 100);
+  camera.lookAt(0, 0, 0);
+
+  const scene = new THREE.Scene();
+  const renderer = new THREE.CSS3DRenderer();
+  const textElem = document.getElementById("info");
+  const object = new THREE.CSS3DObject(textElem);
+
+  object.position.set(0, 1, -5);
+  scene.add(object);
+}
+
+// TextRenderer3d();
