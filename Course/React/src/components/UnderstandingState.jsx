f344bdf7bd88398fdaa3d5363c82ce97b76392dc
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-07 21:28:32 +0530

    Understanding how to use the useState hook

---
+++ Course/React/src/components/UnderstandingState.jsx
@@ -0,0 +1,14 @@
+// To understand State we need to use react hooks.
+// useState is a hook that allows us to create a state.
+// How to use useState hook ?
+// 1. Import useState from react
+// 2. Create a state variable
+// 3. Pass the variable to the component
+// 4. Use the variable in the component
+// 5. Update the state variable
+// 6. the useState hook returns an array.
+// The first element of the array is the state variable.
+// The second element of the array is a function that allows us to update the state variable which should be a function.
+// const [name, setName] = useState("John");
+// const [age, setAge] = useState(30);
+// here name and age are the state variables and setName and setAge are the functions that allow us to update the state variables.
