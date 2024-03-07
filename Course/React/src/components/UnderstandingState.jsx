4c019d5f48c551d4f0ebf03e60f8db6017f2709d
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-07 22:22:37 +0530

    React useState advanced done - how to handle arrays and objects

--- Course/React/src/components/UnderstandingState.jsx
+++ Course/React/src/components/UnderstandingState.jsx
@@ -18,31 +18,53 @@
 // For eg- if we want to increment the age by 1 then we can do this.
 // setAge((prevAge) => prevAge + 1);
 
+// When we talk about reference type variables inside of setState, we can only change the values by using the spread operator and not by directly changing the variables value.
+// What does this mean ? Let's see an example
+
 import React, { useState } from "react";
 
 function UnderstandingState() {
   const [counter, setCounter] = useState(0);
+  const [obj, setObj] = useState({ name: "Abhishek", isBanned: false });
   function handleDecrease() {
     if (counter > 0) setCounter(counter - 1);
   }
   return (
-    <div className="p-4">
-      <h1 className="text-lime-900 text-4xl">{counter}</h1>
-      <button
-        className="bg-blue-800 text-white py-3 px-2 mt-4 mr-4 rounded-xl"
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        Increase Counter
-      </button>{" "}
-      <button
-        className="bg-blue-800 text-white py-3 px-2 mt-4 rounded-xl"
-        onClick={handleDecrease}
-      >
-        Decrease Counter
-      </button>
-    </div>
+    <>
+      <div className="p-4">
+        <h1 className="text-lime-900 text-4xl">{counter}</h1>
+        <button
+          className="bg-blue-800 text-white py-3 px-2 mt-4 mr-4 rounded-xl"
+          onClick={() => {
+            setCounter((prev) => prev + 1);
+          }}
+        >
+          Increase Counter
+        </button>{" "}
+        <button
+          className="bg-blue-800 text-white py-3 px-2 mt-4 rounded-xl"
+          onClick={handleDecrease}
+        >
+          Decrease Counter
+        </button>
+      </div>
+      <div className="p-4">
+        <h1 className="text-lime-900 text-4xl"> Name : {obj.name}</h1>
+        <h1 className="text-lime-900 text-4xl">
+          {" "}
+          Banned : {obj.isBanned.toString()}
+        </h1>
+        <button
+          className="bg-blue-800 text-white py-3 px-2 mt-4 mr-4 rounded-xl"
+          onClick={() => {
+            setObj({ ...obj, isBanned: !obj.isBanned });
+          }}
+        >
+          {/* This way we can change the behaviour of the obj and similar to this is what we can do with arrays */}
+          Change
+        </button>{" "}
+      </div>
+    </>
   );
 }
 
