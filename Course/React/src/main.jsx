35ea68b06e89b3740d40f696dd8e3c32253ef98b
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-08 06:29:49 +0530

    Will play with objects later

--- Course/React/src/main.jsx
+++ Course/React/src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import ConditionalRendering from "./components/ConditionalRendering.jsx";
 import UnderstandingState from "./components/UnderstandingState.jsx";
+import StateWithObjectsAndArrays from "./components/StateWithObjectsAndArrays.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <ConditionalRendering /> */}
-    <UnderstandingState />
+    {/* <UnderstandingState /> */}
+    <StateWithObjectsAndArrays />
   </React.StrictMode>
 );
