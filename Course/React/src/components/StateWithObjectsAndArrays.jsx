982e892b5626b09d4ccba6dfa32b4c2e508d2280
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-09 23:19:56 +0530

    Created a new array for better understanding

--- Course/React/src/components/StateWithObjectsAndArrays.jsx
+++ Course/React/src/components/StateWithObjectsAndArrays.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function StateWithObjectsAndArrays() {
   const [val, setVal] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [val1, setVal1] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   const [obj, setObj] = useState([
     { name: "Abhishek", age: 20, isBanned: false },
     { name: "Sachin", age: 21, isBanned: true },
@@ -35,11 +36,13 @@ function StateWithObjectsAndArrays() {
             <h1 className="text-orange-200 text-3xl">
               Delete specific element <br /> when delete button is clicked
             </h1>
-            {val.map((el, index) => (
+            {val1.map((el, index) => (
               <div key={index} className="flex items-center gap-2">
                 <h1 className="text-orange-700 text-4xl">{el}</h1>
                 <button
-                  onClick={() => setVal(val.filter((_, idx) => idx !== index))}
+                  onClick={() =>
+                    setVal1(val1.filter((_, idx) => idx !== index))
+                  }
                   className="w-40 h-9 text-white bg-slate-800"
                 >
                   Delete
