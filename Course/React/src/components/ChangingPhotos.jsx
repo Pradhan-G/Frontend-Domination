7d54dbe292790c7c295c28ebf37dbaa50c61e5a0
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-10 23:55:54 +0530

    added some animations to the changing photos component

--- Course/React/src/components/ChangingPhotos.jsx
+++ Course/React/src/components/ChangingPhotos.jsx
@@ -9,28 +9,28 @@ function ChangingPhotos() {
         <img
           className={`h-full w-full ${
             count === 0 ? "translate-x-[0%]" : "-translate-x-[100%]"
-          }  object-cover flex-shrink-0`}
+          }  object-cover flex-shrink-0 transition-transform delay-700`}
           src="https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <img
           className={`h-full w-full ${
-            count === 1 ? "-translate-x-[100%]" : ""
-          } object-cover flex-shrink-0 `}
+            count === 1 ? "-translate-x-[100%]" : "-translate-x-[200%]"
+          } object-cover flex-shrink-0 transition-transform delay-700 `}
           src="https://plus.unsplash.com/premium_photo-1675368244123-082a84cf3072?q=80&w=2150&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <img
           className={`h-full w-full ${
-            count === 2 ? "-translate-x-[200%]" : ""
-          } object-cover flex-shrink-0`}
+            count === 2 ? "-translate-x-[200%]" : "-translate-x-[300%]"
+          } object-cover flex-shrink-0 transition-transform delay-700`}
           src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <img
           className={`h-full w-full ${
-            count === 3 ? "-translate-x-[300%]" : ""
-          } object-cover flex-shrink-0`}
+            count === 3 ? "-translate-x-[300%]" : "-translate-x-[400%]"
+          } object-cover flex-shrink-0 transition-transform delay-700`}
           src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
