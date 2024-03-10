baf5664c6f517f0b51ad9dd97f64ea3703768d6e
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-10 23:49:18 +0530

    Mistakes corrected in the changing photos component.

--- Course/React/src/components/ChangingPhotos.jsx
+++ Course/React/src/components/ChangingPhotos.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
 function ChangingPhotos() {
   const [count, setCount] = useState(0);
+  var flag = true;
   return (
     <div className="w-screen h-screen bg-zinc-700 flex justify-center items-center">
       <div className="w-1/2 h-1/2 bg-slate-900 rounded-md overflow-hidden flex relative">
@@ -14,21 +15,21 @@ function ChangingPhotos() {
         />
         <img
           className={`h-full w-full ${
-            count === 1 ? "" : "-translate-x-[100%]"
-          } object-cover flex-shrink-0`}
+            count === 1 ? "-translate-x-[100%]" : ""
+          } object-cover flex-shrink-0 `}
           src="https://plus.unsplash.com/premium_photo-1675368244123-082a84cf3072?q=80&w=2150&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <img
           className={`h-full w-full ${
-            count === 2 ? "" : "-translate-x-[200%]"
+            count === 2 ? "-translate-x-[200%]" : ""
           } object-cover flex-shrink-0`}
           src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
         <img
           className={`h-full w-full ${
-            count === 3 ? "" : "-translate-x-[300%]"
+            count === 3 ? "-translate-x-[300%]" : ""
           } object-cover flex-shrink-0`}
           src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
