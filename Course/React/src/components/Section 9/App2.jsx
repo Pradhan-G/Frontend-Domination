59e3a22cb89f2db900424fe858c5506d320a00d2
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-18 05:20:35 +0530

    Section 9 progress

--- Course/React/src/components/Section 9/App2.jsx
+++ Course/React/src/components/Section 9/App2.jsx
@@ -3,6 +3,27 @@ import Card from "./Card";
 import Navbar from "./Navbar";
 
 function App2() {
+  const data = [
+    {
+      song: "Channa Mereya",
+      artist: "Arijit Singh",
+      img: "src/assets/images/1.webp",
+    },
+    { song: "Maula", artist: "Anuv Jain", img: "src/assets/images/2.webp" },
+    { song: "Maayeri", artist: "Palash Sen", img: "src/assets/images/3.webp" },
+    { song: "Rap God", artist: "Eminem", img: "src/assets/images/4.webp" },
+    {
+      song: "Papa Meri Jaan",
+      artist: "Sonu Nigam",
+      img: "src/assets/images/5.webp",
+    },
+    {
+      song: "O Saathi",
+      artist: "Atif Aslam",
+      img: "src/assets/images/6.webp",
+    },
+  ];
+
   return (
     <div className="bg-slate-900 w-full h-screen text-white">
       <Navbar />
--- Course/React/src/components/Section 9/Card.jsx
+++ Course/React/src/components/Section 9/Card.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function Card() {
+function Card({ img, artistName, songName, added }) {
   return (
-    <div className="">
-      <div></div>
+    <div className="bg-gray-500 h-[100px] w-[350px] rounded-lg flex gap-5 justify-between relative z-0">
+      <div className="bg-gray-700 h-full w-1/4 rounded-lg overflow-hidden">
+        <img className="h-full w-full " src={img} alt="" />
+      </div>
+      <div className="h-full w-3/4 flex flex-col">
+        <h1 className="font-bold text-2xl">{songName}</h1>
+        <h1 className="font-semiboldbold text-1xl">{artistName}</h1>
+      </div>
+      <button className="absolute bg-orange-600 h-[50px] w-[180px] rounded-lg  translate-x-[-50%] -translate-y-[-50%] bottom-0 z-10 left-1/2 hover:bg-orange-400 transition-all">
+        Add to Favourites
+      </button>
     </div>
   );
 }
