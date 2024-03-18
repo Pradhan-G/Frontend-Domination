ee4760e66a5c00e05dbd78ec6927ac638b6b8cdf
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-19 03:10:56 +0530

    Section 10 Part 2 complete

--- Course/React/src/components/Section 10/App3.jsx
+++ Course/React/src/components/Section 10/App3.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FormHandling1 from "./FormHandling1";
+import FormHandling2 from "./FormHandling2";
 
 function App3() {
   return (
     <div>
       <FormHandling1 />
+      <FormHandling2 />
     </div>
   );
 }
--- Course/React/src/components/Section 10/FormHandling2.jsx
+++ Course/React/src/components/Section 10/FormHandling2.jsx
@@ -1,13 +1,14 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 // This is the way we where handle forms using useRef
 
 function FormHandling2() {
+  let [name, setName] = useState("");
+  let [age, setAge] = useState("");
+  let [email, setEmail] = useState("");
+  const [val, setVal] = useState({ name: "", email: "", age: "" });
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(name.current.value);
-    console.log(age.current.value);
-    console.log(email.current.value);
   };
 
   return (
@@ -15,32 +16,42 @@ function FormHandling2() {
       <form className="m-10" action="#" onSubmit={handleSubmit}>
         <h1 className="mx-2 text-5xl font-semibold underline mb-5">
           {" "}
-          Form - UseRef{" "}
+          Form - Controlled Components{" "}
         </h1>
         <input
           className="border-2 rounded-lg mx-2 p-2"
-          ref={name}
           type="text"
           placeholder="Enter Name"
+          onChange={(event) => {
+            setVal({ ...val, name: event.target.value });
+          }}
         />
         <input
           className="border-2 rounded-lg mx-2 p-2"
-          ref={age}
           type="number"
           placeholder="Enter Age"
+          onChange={(event) => {
+            setVal({ ...val, age: event.target.value });
+          }}
         />
         <input
           className="border-2 rounded-lg mx-2 p-2"
-          ref={email}
           type="email"
           placeholder="Enter Email"
+          onChange={(event) => {
+            setVal({ ...val, email: event.target.value });
+          }}
         />
         <button className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg">
           Submit
         </button>
       </form>
 
-      <div>Name - Age - Email -</div>
+      <div className="px-10">
+        <h1 className="text-3xl font-semibold"> Name - {val.name} </h1>
+        <h1 className="text-3xl font-semibold"> Age - {val.age} </h1>
+        <h1 className="text-3xl font-semibold"> Email - {val.email} </h1>
+      </div>
     </div>
   );
 }
