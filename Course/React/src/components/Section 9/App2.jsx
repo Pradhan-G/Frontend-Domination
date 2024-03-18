dc8d9f8735c7756dde124a14dc0a678180bc9183
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-18 19:41:54 +0530

    Section 9 progress

--- Course/React/src/components/Section 9/App2.jsx
+++ Course/React/src/components/Section 9/App2.jsx
@@ -1,33 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import Navbar from "./Navbar";
 
 function App2() {
+  let counter = 0;
+
   const data = [
     {
       song: "Channa Mereya",
       artist: "Arijit Singh",
       img: "src/assets/images/1.webp",
+      added: false,
     },
-    { song: "Maula", artist: "Anuv Jain", img: "src/assets/images/2.webp" },
-    { song: "Maayeri", artist: "Palash Sen", img: "src/assets/images/3.webp" },
-    { song: "Rap God", artist: "Eminem", img: "src/assets/images/4.webp" },
+    {
+      song: "Maula",
+      artist: "Anuv Jain",
+      img: "src/assets/images/2.webp",
+      added: false,
+    },
+    {
+      song: "Maayeri",
+      artist: "Palash Sen",
+      img: "src/assets/images/3.webp",
+      added: false,
+    },
+    {
+      song: "Rap God",
+      artist: "Eminem",
+      img: "src/assets/images/4.webp",
+      added: false,
+    },
     {
       song: "Papa Meri Jaan",
       artist: "Sonu Nigam",
       img: "src/assets/images/5.webp",
+      added: false,
     },
     {
       song: "O Saathi",
       artist: "Atif Aslam",
       img: "src/assets/images/6.webp",
+      added: false,
     },
   ];
 
+  const [realData, setRealData] = useState(data);
+
+  const handleClick = (index) => {
+    setRealData((prev) => {
+      return prev.map((item, i) => {
+        if (index === i) {
+          counter += 1;
+          return { ...item, added: !item.added };
+        } else {
+          return item;
+        }
+      });
+    });
+  };
+
   return (
     <div className="bg-slate-900 w-full h-screen text-white">
-      <Navbar />
-      <Card />
+      <Navbar counter={counter} />
+      <div className="flex flex-wrap gap-20 mt-10">
+        {realData.map((item, index) => {
+          return (
+            <Card
+              img={item.img}
+              artistName={item.artist}
+              songName={item.song}
+              added={item.added}
+              handleClick={handleClick}
+              index={index}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- Course/React/src/components/Section 9/Card.jsx
+++ Course/React/src/components/Section 9/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card({ img, artistName, songName, added }) {
+function Card({ img, artistName, songName, added, handleClick, index }) {
   return (
     <div className="bg-gray-500 h-[100px] w-[350px] rounded-lg flex gap-5 justify-between relative z-0">
       <div className="bg-gray-700 h-full w-1/4 rounded-lg overflow-hidden">
@@ -10,8 +10,13 @@ function Card({ img, artistName, songName, added }) {
         <h1 className="font-bold text-2xl">{songName}</h1>
         <h1 className="font-semiboldbold text-1xl">{artistName}</h1>
       </div>
-      <button className="absolute bg-orange-600 h-[50px] w-[180px] rounded-lg  translate-x-[-50%] -translate-y-[-50%] bottom-0 z-10 left-1/2 hover:bg-orange-400 transition-all">
-        Add to Favourites
+      <button
+        onClick={() => {
+          handleClick(index);
+        }}
+        className="absolute bg-orange-600 h-[50px] w-[180px] rounded-lg  translate-x-[-50%] -translate-y-[-50%] bottom-0 z-10 left-1/2 hover:bg-orange-400 transition-all"
+      >
+        {added ? "Added" : "Add to Favourites"}
       </button>
     </div>
   );
