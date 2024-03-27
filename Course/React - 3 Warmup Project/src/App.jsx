420fb606e73ac07a7e5e9e93b7c949ee6a7fea19
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-27 12:13:38 +0530

    Created a React app and initialised tailwind

---
+++ Course/React - 3 Warmup Project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-4xl container mx-auto p-2 bg-red-100 text-center">
+      App
+    </div>
+  );
+};
+
+export default App;
